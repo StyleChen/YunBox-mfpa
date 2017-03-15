e9818bcf76d13a5c1af478c70000f16178af0fc3
StyleChen <chen_jian_x@sina.com>
2017-03-15 17:40:30 +0800

    2017-3-15 17:37

--- pro1/index.php
+++ pro1/index.php
@@ -17,7 +17,7 @@
                 <div class="item active">
                     <img src="images/banner1.png" alt="...">
                     <div class="carousel-caption">
-                        ...
+
                     </div>
                 </div>
                 <div class="item">
@@ -69,6 +69,20 @@
                             <span class="right">2016-12-01</span>
                         </a>
                     </li>
+                    <li>
+                        <a class="news-a" href="#">
+                            <i class="left"></i>
+                            <span class="left">口足绘画艺术家协会举办会展</span>
+                            <span class="right">2016-12-01</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="news-a" href="#">
+                            <i class="left"></i>
+                            <span class="left">口足绘画艺术家协会举办会展</span>
+                            <span class="right">2016-12-01</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
             <div class="art">
@@ -80,24 +94,32 @@
                 </div>
                 <div class="flex text-center">
                     <dl>
-                        <dt><img src="images/shuhai.jpg" alt="树海"></dt>
-                        <dd><h4>树海</h4></dd>
+                        <dt><img src="images/shuhai.png" alt="树海"></dt>
+                        <dd><h4 class="left">[ 树海 ]</h4><h4 class="right">赵树海</h4></dd>
                     </dl>
                     <dl>
-                        <dt><img src="images/huahai.jpg" alt="花海"></dt>
-                        <dd><h4>花海</h4></dd>
+                        <dt><img src="images/shuhai.png" alt="花海"></dt>
+                        <dd><h4 class="left">[ 花海 ]</h4><h4 class="right">赵树海</h4></dd>
                     </dl>
                     <dl>
-                        <dt><img src="images/old1.jpg" alt="老街"></dt>
-                        <dd><h4>老街</h4></dd>
+                        <dt><img src="images/shuhai.png" alt="花海"></dt>
+                        <dd><h4 class="left">[ 花海 ]</h4><h4 class="right">赵树海</h4></dd>
                     </dl>
                     <dl>
-                        <dt><img src="images/old2.jpg" alt="老街"></dt>
-                        <dd><h4>老街</h4></dd>
+                        <dt><img src="images/old3.png" alt="老街"></dt>
+                        <dd><h4 class="left">[ 老街 ]</h4><h4 class="right">赵树海</h4></dd>
                     </dl>
                     <dl>
-                        <dt><img src="images/old3.jpg" alt="老街"></dt>
-                        <dd><h4>老街</h4></dd>
+                        <dt><img src="images/old3.png" alt="老街"></dt>
+                        <dd><h4 class="left">[ 老街 ]</h4><h4 class="right">赵树海</h4></dd>
+                    </dl>
+                    <dl>
+                        <dt><img src="images/old3.png" alt="老街"></dt>
+                        <dd><h4 class="left">[ 老街 ]</h4><h4 class="right">赵树海</h4></dd>
+                    </dl>
+                    <dl>
+                        <dt><img src="images/old3.png" alt="老街"></dt>
+                        <dd><h4 class="left">[ 老街 ]</h4><h4 class="right">赵树海</h4></dd>
                     </dl>
                 </div>
             </div>
@@ -110,10 +132,12 @@
                 </div>
                 <div class="">
                     <div>
-                        <h3 class="text-center">儿童绘画比赛</h3>
                         <dl>
                             <dt class="col-md-6 col-xs-12"><img src="images/hot1.png" alt=""></dt>
-                            <dd class="col-md-6">华东师范湖南电视剧俯拾地芥佛挡杀佛十大十大发送到焚枯食淡十大水电费华东师范湖南电视剧俯拾地芥佛挡杀佛十大十大发送到焚枯食淡十大水电费华东师范湖南电视剧俯拾地芥佛挡杀佛十大十大发送到焚枯食淡十大水电费</dd>
+                            <dd class="col-md-6 col-xs-12">
+                                <h3 class="text-center">儿童绘画比赛</h3>
+                                <p>华东师范湖南电视剧俯拾地芥佛挡杀佛十大十大发送到焚枯食淡十大水电费华东师范湖南电视剧俯拾地芥佛挡杀佛十大十大发送到焚枯食淡十大水电费华东师范湖南电视剧俯拾地芥佛挡杀佛十大十大发送到焚枯食淡十大水电费</p>
+                            </dd>
                         </dl>
                     </div>
                 </div>
