723a2f89063a6cdf8a0fc225f9d347ecff69bb42
vivlong <vivlonglz@gmail.com>
2017-03-20 15:09:53 +0800

    Update

---
+++ personal-page.php
@@ -0,0 +1,29 @@
+
+<style>
+    .login-wrap a{color:#333;}
+    .login-wrap a:hover{color:#FF5864;}
+    html{
+        font-size:16px;
+    }
+    body{
+        background: none;
+    }
+</style>
+<section class="section-bg">
+    <div class="right login text-center">
+        <h4 class="text-left">账号登录</h4>
+        <div class="login-wrap text-align">
+            <div class="login-public"><span class="user">账号</span><input type="text"></div>
+            <div class="login-public"><span class="password">密码</span><input type="password"></div>
+            <button>登录</button>
+            <a class="login-a" href="#">免费注册</a>
+        </div>
+        <div class="login-wrap text-align hide">
+            <div class="login-public"><span class="user">账号</span><input type="text"></div>
+            <div class="login-public"><span class="password">密码</span><input type="password"></div>
+            <div class="login-public"><span class="password">确认</span><input type="password"></div>
+            <button>免费注册</button>
+            <a class="login-a" href="#">已有账号,立即登录</a>
+        </div>
+    </div>
+</section>
